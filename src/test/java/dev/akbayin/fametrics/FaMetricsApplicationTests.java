72dab825467cf87115f769f1b559565a76446175
Umut Akbayin <umutakbayin@posteo.de>
2026-08-03 14:52:42 +0200

    test: Add basic Spring Boot application test class

---
+++ src/test/java/dev/akbayin/fametrics/FaMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.akbayin.fametrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
