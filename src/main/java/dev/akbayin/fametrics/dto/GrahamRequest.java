b555364eec6a9fbe0c3e9a4980b001f56fa79f46
Umut Akbayin <umutakbayin@posteo.de>
2026-08-06 10:47:24 +0200

    feat: Update GrahamRequest DTO to use @Positive for validation annotations

--- src/main/java/dev/akbayin/fametrics/dto/GrahamRequest.java
+++ src/main/java/dev/akbayin/fametrics/dto/GrahamRequest.java
@@ -1,12 +1,12 @@
 package dev.akbayin.fametrics.dto;
 
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
 public record GrahamRequest(
-    @NotNull @DecimalMin(value = "0.0", inclusive = false) BigDecimal eps,
-    @NotNull @DecimalMin(value = "0.0", inclusive = false) BigDecimal bvps
+    @NotNull @Positive BigDecimal eps,
+    @NotNull @Positive BigDecimal bvps
 ) {
 }
