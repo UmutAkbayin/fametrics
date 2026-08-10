df9e3878993f20102e08492fae5aaeadc892dc6f
Umut Akbayin <umutakbayin@posteo.de>
2026-08-10 07:31:01 +0200

    refactor: Update summary endpoint to return SummaryResponse instead of BigDecimal

--- src/main/java/dev/akbayin/fametrics/controller/ValuationController.java
+++ src/main/java/dev/akbayin/fametrics/controller/ValuationController.java
@@ -9,6 +9,7 @@ import dev.akbayin.fametrics.dto.PegRatioRequest;
 import dev.akbayin.fametrics.dto.PsRatioRequest;
 import dev.akbayin.fametrics.dto.RoeRequest;
 import dev.akbayin.fametrics.dto.SummaryRequest;
+import dev.akbayin.fametrics.dto.SummaryResponse;
 import dev.akbayin.fametrics.service.ValuationService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -84,7 +85,7 @@ public class ValuationController {
     }
 
     @PostMapping("/summary")
-    public ResponseEntity<BigDecimal> getSummary(@Valid @RequestBody SummaryRequest request) {
+    public ResponseEntity<SummaryResponse> getSummary(@Valid @RequestBody SummaryRequest request) {
         return valuationService.calculateSummary(request)
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.unprocessableContent().build());
