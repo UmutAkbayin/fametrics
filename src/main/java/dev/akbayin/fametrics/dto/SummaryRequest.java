9923c0ef425ab963b2d1c8aa6c11ab26fe68ab10
Umut Akbayin <umutakbayin@posteo.de>
2026-08-17 11:13:07 +0200

    refactor: Consolidate properties in SummaryRequest into grouped DTOs

--- src/main/java/dev/akbayin/fametrics/dto/SummaryRequest.java
+++ src/main/java/dev/akbayin/fametrics/dto/SummaryRequest.java
@@ -3,14 +3,8 @@ package dev.akbayin.fametrics.dto;
 import java.math.BigDecimal;
 
 public record SummaryRequest(
-    BigDecimal sharePrice,
-    BigDecimal eps,
-    BigDecimal bvps,
-    BigDecimal marketCap,
-    BigDecimal totalRevenue,
-    BigDecimal epsGrowthRate,
-    BigDecimal totalLiabilities,
-    BigDecimal totalEquity,
-    BigDecimal netIncome
+    MarketData marketData,
+    FundamentalData fundamentalData,
+    CapitalStructure capitalStructure
     ) {
 }
